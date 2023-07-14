060cca40876055177977cf48733638b683f91813
parkikbum <pibum@naver.com>
2023-07-15 00:13:13 +0900

    ➕[ADD]: 아이콘 png -> svg 로 변경 및 네비게이션 타이틀 변경

--- Sources/SDSKit/Icon/SDSIcon.swift
+++ Sources/SDSKit/Icon/SDSIcon.swift
@@ -3,30 +3,23 @@ import UIKit
 #endif
 
 public enum SDSIcon {
-    public static var icAddCircle16: UIImage { .load(name: "icAddCircle16") }
-    public static var icAddCircle20: UIImage { .load(name: "icAddCircle20") }
+    public static var icAddCircle: UIImage { .load(name: "icAddCircle") }
     public static var icAlertActive: UIImage { .load(name: "icAlertActive") }
     public static var icAlertDefault: UIImage { .load(name: "icAlertDefault") }
     public static var icAppleLogin: UIImage { .load(name: "icAppleLogin") }
-    public static var icBackArrow36: UIImage { .load(name: "icBackArrow36") }
-    public static var icCheckmarkFill24: UIImage { .load(name: "icCheckmarkFill24") }
-    public static var icCheckmarkFill32: UIImage { .load(name: "icCheckmarkFill32") }
-    public static var icCheckmarkUnfill24: UIImage { .load(name: "icCheckmarkUnfill24") }
-    public static var icCheckmarkUnfill32: UIImage { .load(name: "icCheckmarkUnfill32") }
-    public static var icChevron20: UIImage { .load(name: "icChevron20") }
-    public static var icChevron24: UIImage { .load(name: "icChevron24") }
-    public static var icChevron32: UIImage { .load(name: "icChevron32") }
-    public static var icChevron36: UIImage { .load(name: "icChevron36") }
+    public static var icCheckmarkFill: UIImage { .load(name: "icCheckmarkFill") }
+    public static var icCheckmarkUnfill: UIImage { .load(name: "icCheckmarkUnfill") }
+    public static var icChevron: UIImage { .load(name: "icChevron") }
     public static var icChevronLeft: UIImage { .load(name: "icChevronLeft") }
     public static var icDismiss: UIImage { .load(name: "icDismiss") }
     public static var icGooleLogin: UIImage { .load(name: "icGooleLogin") }
-    public static var icInfo: UIImage { .load(name: "icInfo") }
+    public static var icHeartEmpty: UIImage { .load(name: "icHeartEmpty") }
+    public static var icHeartFill: UIImage { .load(name: "icHeartFill") }
     public static var icKakaoLogin: UIImage { .load(name: "icKakaoLogin") }
+    public static var icLogo: UIImage { .load(name: "icLogo") }
     public static var icPerson: UIImage { .load(name: "icPerson") }
     public static var icReset: UIImage { .load(name: "icReset") }
-    public static var icShare20: UIImage { .load(name: "icShare20") }
-    public static var icShare24: UIImage { .load(name: "icShare24") }
-    public static var icHeartFill: UIImage { .load(name: "icHeartFill") }
-    public static var icHeartEmpty: UIImage { .load(name: "icHeartEmpty") }
+    public static var icSad: UIImage { .load(name: "icSad") }
+    public static var icShare: UIImage { .load(name: "icShare") }
     
 }
